7b0ea11bebca2c97bf9498118ae70aaff1c16851
SinZ <github@361zn.is>
2023-10-15 11:49:09 +1100

    Initial development on a data generation tool + analyzer for detecting misuse of ContentManagers

---
+++ src/SMAPI.ModBuildConfig.Analyzer.Tests/ContentManagerAnalyzerTests.cs
@@ -0,0 +1,90 @@
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using NUnit.Framework;
+using SMAPI.ModBuildConfig.Analyzer.Tests.Framework;
+using StardewModdingAPI.ModBuildConfig.Analyzer;
+
+namespace SMAPI.ModBuildConfig.Analyzer.Tests
+{
+    /// <summary>Unit tests for <see cref="ContentManagerAnalyzer"/>.</summary>
+    [TestFixture]
+    public class ContentManagerAnalyzerTests : DiagnosticVerifier
+    {
+        /*********
+        ** Fields
+        *********/
+        /// <summary>Sample C# mod code, with a {{test-code}} placeholder for the code in the Entry method to test.</summary>
+        const string SampleProgram = @"
+            using System;
+            using System.Collections.Generic;
+            using StardewValley;
+            using Netcode;
+            using SObject = StardewValley.Object;
+
+            namespace SampleMod
+            {
+                class ModEntry
+                {
+                    public void Entry()
+                    {
+                        {{test-code}}
+                    }
+                }
+            }
+        ";
+
+        /// <summary>The line number where the unit tested code is injected into <see cref="SampleProgram"/>.</summary>
+        private const int SampleCodeLine = 14;
+
+        /// <summary>The column number where the unit tested code is injected into <see cref="SampleProgram"/>.</summary>
+        private const int SampleCodeColumn = 25;
+
+
+        /*********
+        ** Unit tests
+        *********/
+        /// <summary>Test that no diagnostics are raised for an empty code block.</summary>
+        [TestCase]
+        public void EmptyCode_HasNoDiagnostics()
+        {
+            // arrange
+            string test = @"";
+
+            // assert
+            this.VerifyCSharpDiagnostic(test);
+        }
+
+        /// <summary>Test that the expected diagnostic message is raised for avoidable net field references.</summary>
+        /// <param name="codeText">The code line to test.</param>
+        /// <param name="column">The column within the code line where the diagnostic message should be reported.</param>
+        /// <param name="expression">The expression which should be reported.</param>
+        /// <param name="netType">The net type name which should be reported.</param>
+        /// <param name="suggestedProperty">The suggested property name which should be reported.</param>
+        [TestCase("Game1.content.Load<Dictionary<int, string>>(\"Data\\\\Fish\");", 0, "Data\\Fish", "System.Collections.Generic.Dictionary<System.Int32, System.String>", "System.Collections.Generic.Dictionary<System.String,System.String>")]
+        public void BadType_RaisesDiagnostic(string codeText, int column, string assetName, string expectedType, string suggestedType)
+        {
+            // arrange
+            string code = SampleProgram.Replace("{{test-code}}", codeText);
+            DiagnosticResult expected = new()
+            {
+                Id = "AvoidContentManagerBadType",
+                Message = $"'{assetName}' uses the {suggestedType} type, but {expectedType} is in use instead. See https://smapi.io/package/avoid-contentmanager-type for details.",
+                Severity = DiagnosticSeverity.Error,
+                Locations = new[] { new DiagnosticResultLocation("Test0.cs", SampleCodeLine, SampleCodeColumn + column) }
+            };
+
+            // assert
+            this.VerifyCSharpDiagnostic(code, expected);
+        }
+
+
+        /*********
+        ** Helpers
+        *********/
+        /// <summary>Get the analyzer being tested.</summary>
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new ContentManagerAnalyzer();
+        }
+    }
+}
---
+++ src/SMAPI.ModBuildConfig.Analyzer/ContentManagerAnalyzer.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis;
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Reflection;
+using System.IO;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+
+namespace StardewModdingAPI.ModBuildConfig.Analyzer
+{
+    public class VersionModel
+    {
+        public VersionModel(Dictionary<string, string> assetMap, HashSet<string> missingAssets)
+        {
+            this.AssetMap = assetMap;
+            this.FormerAssets = missingAssets;
+        }
+
+        public Dictionary<string, string> AssetMap { get; set; }
+        public HashSet<string> FormerAssets { get; set; }
+    }
+
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class ContentManagerAnalyzer : DiagnosticAnalyzer
+    {
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; }
+        public VersionModel OneSixRules { get; }
+
+
+        /// <summary>The diagnostic info for an avoidable net field access.</summary>
+        private readonly DiagnosticDescriptor AvoidBadTypeRule = new(
+            id: "AvoidContentManagerBadType",
+            title: "Avoid incorrectly typing ContentManager Loads",
+            messageFormat: "'{0}' uses the {1} type, but {2} is in use instead. See https://smapi.io/package/avoid-contentmanager-type for details.",
+            category: "SMAPI.CommonErrors",
+            defaultSeverity: DiagnosticSeverity.Error,
+            isEnabledByDefault: true,
+            helpLinkUri: "https://smapi.io/package/avoid-contentmanager-type"
+        );
+
+        public ContentManagerAnalyzer()
+        {
+            using (Stream stream = this.GetType().Assembly.GetManifestResourceStream("StardewModdingAPI.ModBuildConfig.Analyzer.1.6.0.23268.json"))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                this.OneSixRules = JsonConvert.DeserializeObject<VersionModel>(reader.ReadToEnd());
+            }
+            this.SupportedDiagnostics = ImmutableArray.CreateRange(new[] { this.AvoidBadTypeRule });
+        }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+            context.EnableConcurrentExecution();
+
+            context.RegisterSyntaxNodeAction(
+                this.AnalyzeContentManagerLoads,
+                SyntaxKind.InvocationExpression
+            );
+        }
+
+        private void AnalyzeContentManagerLoads(SyntaxNodeAnalysisContext context)
+        {
+            var invocation = (InvocationExpressionSyntax)context.Node;
+            var memberAccess = invocation.Expression as MemberAccessExpressionSyntax;
+            if (memberAccess == null || memberAccess.Name.Identifier.ValueText != "Load")
+                return;
+            // "Data\\Fish" -> Data\Fish
+            string assetName = invocation.ArgumentList.Arguments[0].ToString().Replace("\"", "").Replace("\\\\", "\\");
+
+            var formatter = new SymbolDisplayFormat(typeQualificationStyle: SymbolDisplayTypeQualificationStyle.NameAndContainingTypesAndNamespaces, genericsOptions: SymbolDisplayGenericsOptions.IncludeTypeParameters);
+            string genericArgument = context.SemanticModel.GetTypeInfo((memberAccess.Name as GenericNameSyntax).TypeArgumentList.Arguments[0]).Type.ToDisplayString(formatter);
+
+            if (this.OneSixRules.AssetMap.TryGetValue(assetName, out string expectedType))
+            {
+                expectedType = Regex.Replace(expectedType, "`\\d+", "");
+                if (genericArgument != expectedType)
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(this.AvoidBadTypeRule, context.Node.GetLocation(), assetName, expectedType, genericArgument));
+                }
+            }
+        }
+    }
+}
